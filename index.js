8e6ce5ea7e76ef566b6b0f58e94403bc27c67c07
Leonard18aa <67755595+Leonard18aa@users.noreply.github.com>
2022-06-21 14:40:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,134 +1,40 @@
+// Instructions
 // - Create variables and/or calculate:
-// - The gross salary. Use 500 EUR for now.
-// - Employee pension contribution, use 5% if not sure 🙂
-//   - Taxable amount (salary minus pension)
-// - Now the #fun part - calculate how much the person is taxed based on:
-//   - 0% tax on 0-80 EUR per month
-//   - 4% on amounts between 80-250 EUR
-//   - 8% on amounts between 250-450 EUR
-//   - 10% on everything over 450 EUR.
+//   - The gross salary. Use 500 EUR for now.
+//   - Employee pension contribution, use 5% if not sure 🙂 
+//     - Taxable amount (salary minus pension)
+//   - Now the #fun part - calculate how much the person is taxed based on:
+//     - 0% tax on 0-80 EUR per month
+//     - 4% on amounts between 80-250 EUR
+//     - 8% on amounts between 250-450 EUR
+//     - 10% on everything over 450 EUR.
 // - Calculate the take-home pay by subtracting the tax from the taxable income
 // - Output the final result to the console. [Come up with a nice understandable message]
 
 // Get the app to work with the salary of 500 EUR first
 
-// calculating the gross and taxable salary in Euros
+let payment = 500
+console.log("Your salary is " + payment)
+let EmployeePensionContribution = 0.05 
+let taxableAmount = payment * EmployeePensionContribution
+console.log("You will be taxed " + taxableAmount + "for the Employee pension contribution")
 
-let salary = 1000
-let employeeContributionRate = 0.05
-let pension = salary * employeeContributionRate
-let taxableSalary = salary - pension
 
-// calculating the tax rates
-let taxRateBetween80And250 = 0.04
-let taxRateBetween250And450 = 0.08
-let taxRateOver450 = 0.1
+let for80to250 = 0.04
+let tax4percent = 170
+let total1 = tax4percent * for80to250
+console.log("From 80-250 euro tou will be taxed " + total1)
+let for250to450 = 0.08
+let tax8percent = 200
+let total2 = tax8percent * for250to450
+console.log("From 250-450 euro you will be taxed " + total2)
+let for450over = 0.1
+let tax10percent = 25
+let total3 = tax10percent * for450over
+console.log("From 450 euro over you will be taxed " + total3)
 
-// calculating the tax
-let taxBetween80And250
-if (taxableSalary >= 250) {
-  taxBetween80And250 = 170 * taxRateBetween80And250
-} else if (taxableSalary > 80) {
-  taxBetween80And250 = (taxableSalary - 80) * taxRateBetween80And250
-} else {
-  taxBetween80And250 = 0
-}
+let totalTaxes = total1 + total2 + total3
+console.log("Your total taxes are " + totalTaxes)
 
-let taxBetween250And450
-if (taxableSalary >= 450) {
-  taxBetween250And450 = 200 * taxRateBetween250And450
-} else if (taxableSalary > 250) {
-  taxBetween250And450 = (taxableSalary - 250) * taxRateBetween250And450
-} else {
-  taxBetween250And450 = 0
-}
-
-let taxOver450
-if (taxableSalary > 450) {
-  taxOver450 = (taxableSalary - 450) * taxRateOver450
-} else {
-  taxOver450 = 0
-}
-
-let totalTaxes = taxBetween80And250 + taxBetween250And450 + taxOver450
-
-// calculating the net salary
-let netSalary = salary - pension - totalTaxes
-
-console.log(`
-    Gross salary: ${salary.toFixed(2)}€
-    Employee contribution: ${pension.toFixed(2)}€
-    Taxable salary: ${taxableSalary.toFixed(2)}€
-  
-    Taxes:
-    - Between 80 and 250: ${taxBetween80And250.toFixed(2)}€
-    - Between 250 and 450: ${taxBetween250And450.toFixed(2)}€
-    - Over 450: ${taxOver450.toFixed(2)}€
-    Total: ${totalTaxes.toFixed(2)}€
-  
-    Net salary: ${netSalary.toFixed(2)}€
-  `)
-
-// function calculateNetSalary(salary) {
-//   let employeeContributionRate = 0.05
-//   let pension = salary * employeeContributionRate
-//   let taxableSalary = salary - pension
-
-//   // calculating the tax rates
-//   let taxRateBetween80And250 = 0.04
-//   let taxRateBetween250And450 = 0.08
-//   let taxRateOver450 = 0.1
-
-//   // calculating the tax
-//   let taxBetween80And250
-//   if (taxableSalary >= 250) {
-//     taxBetween80And250 = 170 * taxRateBetween80And250
-//   } else if (taxableSalary > 80) {
-//     taxBetween80And250 = (taxableSalary - 80) * taxRateBetween80And250
-//   } else {
-//     taxBetween80And250 = 0
-//   }
-
-//   let taxBetween250And450
-//   if (taxableSalary >= 450) {
-//     taxBetween250And450 = 200 * taxRateBetween250And450
-//   } else if (taxableSalary > 250) {
-//     taxBetween250And450 = (taxableSalary - 250) * taxRateBetween250And450
-//   } else {
-//     taxBetween250And450 = 0
-//   }
-
-//   let taxOver450
-//   if (taxableSalary > 450) {
-//     taxOver450 = (taxableSalary - 450) * taxRateOver450
-//   } else {
-//     taxOver450 = 0
-//   }
-
-//   let totalTaxes = taxBetween80And250 + taxBetween250And450 + taxOver450
-
-//   // calculating the net salary
-//   let netSalary = salary - pension - totalTaxes
-
-//   console.log(`
-//     Gross salary: ${salary.toFixed(2)}€
-//     Employee contribution: ${pension.toFixed(2)}€
-//     Taxable salary: ${taxableSalary.toFixed(2)}€
-
-//     Taxes:
-//     - Between 80 and 250: ${taxBetween80And250.toFixed(2)}€
-//     - Between 250 and 450: ${taxBetween250And450.toFixed(2)}€
-//     - Over 450: ${taxOver450.toFixed(2)}€
-//     Total: ${totalTaxes.toFixed(2)}€
-
-//     Net salary: ${netSalary.toFixed(2)}€
-//   `)
-// }
-
-// let salary = Number(prompt('What is your salary?'))
-
-// if (Number.isNaN(salary)) {
-//   console.log('Please enter a valid salary')
-// } else {
-//   calculateNetSalary(salary)
-// }
+let YourNetPayement = payment - taxableAmount -totalTaxes
+console.log("Your Net payment is " + YourNetPayement)
